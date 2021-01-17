c4c3e72d57800b6efd76c7e5ce3357f27ecdc863
ohriman <dudgh5184@naver.com>
2021-01-17 21:59:00 +0900

    ~#2.0 socketIO start part1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () => {
@@ -17,3 +19,11 @@ const handleListening = () => {
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 1000);
