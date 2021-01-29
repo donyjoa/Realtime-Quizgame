a4bca9b34e0711cc8d076dbdb855920c55d63586
pigmon-air <dudgh5184@naver.com>
2021-01-29 23:35:13 +0900

    auto 설치까지 했음

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,8 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+
+sass.compiler = require("node-sass");
 
 const paths = {
   styles: {
@@ -14,3 +17,5 @@ export function styles() {
     .pipe(sass())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+// 파일이름이 gulpfile인 이유는 gulp가 자동으로 babelrc 파일을 찾아가서 babel 설정대로 자동 코딩해준다.
