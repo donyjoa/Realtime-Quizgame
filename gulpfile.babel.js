f8e405521a19e17702b910da6fd92486f6b2edfd
pigmon-air <dudgh5184@naver.com>
2021-01-31 21:26:30 +0900

    ~#3.1 gulp part2 good

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
 
 sass.compiler = require("node-sass");
 
@@ -8,14 +9,28 @@ const paths = {
   styles: {
     src: "assets/scss/styles.scss",
     dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",
   },
 };
 
-export function styles() {
+function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
 
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
 // 파일이름이 gulpfile인 이유는 gulp가 자동으로 babelrc 파일을 찾아가서 babel 설정대로 자동 코딩해준다.
