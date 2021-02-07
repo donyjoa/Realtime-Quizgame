ff202734a69d03c6711396cf224c49ec0d27b04b
ohriman <dudgh5184@naver.com>
2021-02-07 23:46:13 +0900

    ~#5.0 reFactoring very hard

--- src/socketController.js
+++ src/socketController.js
@@ -2,8 +2,8 @@ import events from "./event";
 
 const socketController = (socket) => {
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log(nickname);
     socket.nickname = nickname;
+    socket.broadcast.emit(events.newUser, { nickname });
   });
 };
 
